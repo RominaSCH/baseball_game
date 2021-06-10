f2d1897326533678f5470826b11273ceef405c7c
Romina Schann <cuqi213@gmail.com>
2021-06-10 10:30:22 +0900

    get 요청 처리

--- js/server.js
+++ js/server.js
@@ -1,4 +1,11 @@
 const express  = require("express");
 const app = express();
 
-app.listen();//서버를 위한 기본문법 3줄
+app.listen(8080, function(){
+    console.log("listening on 8080");
+});//서버를 위한 기본문법 3줄
+//.listen(서버띄울 포트번호, 띄운 후 실행할 코드)
+
+app.get("/pet", function(요청, 응답){
+    응답.send("냐앙");
+})
